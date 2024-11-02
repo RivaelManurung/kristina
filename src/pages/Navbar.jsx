1e06c1de4364368e8f2de9203e098b3965a106a0
RivaelManurung <135939684+RivaelManurung@users.noreply.github.com>
2024-11-03 00:31:27 +0700

    porto

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar({ toggleTheme }) {
   return (
     <div className="navbar bg-base-200 shadow-lg fixed top-0 left-0 w-full z-50 transition-all">
       <div className="container mx-auto flex justify-between items-center p-4">
-        <a className="text-3xl font-bold text-primary">Portfolio</a>
+        <a className="text-3xl font-bold text-primary">Portofolio</a>
         <div className="hidden lg:flex space-x-6">
           <a className="hover:text-primary transition-colors" href="#hero">About</a>
           <a className="hover:text-primary transition-colors" href="#projects">Projects</a>
